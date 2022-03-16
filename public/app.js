cd48ee47b02df098fd69723c204eb3c955d6f405
Rochdy <rochdy.bachabi@epitech.eu>
2022-03-16 20:00:09 +0100

    Adding button for led and field for reading temperature and reading humidity

--- public/app.js
+++ public/app.js
@@ -1,27 +1,43 @@
-var dataFloatPath = 'test/float';
-var dataIntPath = 'test/int';
+var dataTemperaturePath = 'test/temperature';
+var dataHumidityPath = 'test/humidity';
+var dataLedPath = 'test/led';
 
 // Get a database reference 
-const databaseFloat = database.ref(dataFloatPath);
-const databaseInt = database.ref(dataIntPath);
+const databaseTemperature = database.ref(dataTemperaturePath);
+const databaseHumidity = database.ref(dataHumidityPath);
+const databaseLed = database.ref(dataLedPath);
 
 // Variables to save database current values
-var floatReading;
-var intReading;
+var temperatureReading;
+var humidityReading;
+var ledReading;
 
 // Attach an asynchronous callback to read the data
-databaseFloat.on('value', (snapshot) => {
-  floatReading = snapshot.val();
-  console.log(floatReading);
-  document.getElementById("reading-float").innerHTML = floatReading;
+databaseTemperature.on('value', (snapshot) => {
+  temperatureReading = snapshot.val();
+  console.log(temperatureReading);
+  document.getElementById("reading-temperature").innerHTML = temperatureReading;
 }, (errorObject) => {
   console.log('The read failed: ' + errorObject.name);
 });
 
-databaseInt.on('value', (snapshot) => {
-  intReading = snapshot.val();
-  console.log(intReading);
-  document.getElementById("reading-int").innerHTML = intReading;
+databaseTemperature.on('value', (snapshot) => {
+  humidityReading = snapshot.val();
+  console.log(humidityReading);
+  document.getElementById("reading-humidity").innerHTML = humidityReading;
 }, (errorObject) => {
   console.log('The read failed: ' + errorObject.name);
 });
+
+document.querySelector('#on-off-led').addEventListener("click", (event) => {
+  event.preventDefault();
+  databaseLed.set(1);
+});
+
+// databaseLed.on('value', (snapshot) => {
+//   ledReading = snapshot.val();
+//   console.log(ledReading);
+//   document.getElementById("reading-Led").innerHTML = ledReading;
+// }, (errorObject) => {
+//   console.log('The read failed: ' + errorObject.name);
+// });
